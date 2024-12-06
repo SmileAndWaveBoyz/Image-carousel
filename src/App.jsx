d5fb687cd6ce724c3a06bd41c6194ebdb91aed5e
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-12-06 17:59:58 +0000

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   const[activeSlide, setActiveSlide] = useState(0)
 
   useEffect(()=> {
-    console.log(carouselRef);
     setImageWidth(carouselRef.current.offsetWidth)
     setXScrollWidth(carouselRef.current.scrollWidth)
     const childElementArray = []
@@ -41,26 +40,7 @@ function App() {
 
   return (
     <div className="carouselContainer">
-      <div className="buttonContainer">
-      <div className="leftButton" onClick={prevSlide} style={{display: activeSlide === 0 ? "none" : "block"}}>
-          <svg className='buttonSVG' width="24" height="24" viewBox="0 0 24 24"> 
-            <polygon points="15.293 3.293 6.586 12 15.293 20.707 16.707 19.293 9.414 12 16.707 4.707 15.293 3.293"/>
-          </svg>
-        </div>
-        <div className="rightButton" onClick={nextSlide} style={{display: xScrollPosition === xScrollWidth - imageWidth ? "none" : "block"}}>
-          <svg className='buttonSVG' width="24" height="24" viewBox="0 0 24 24">
-            <polygon points="7.293 4.707 14.586 12 7.293 19.293 8.707 20.707 17.414 12 8.707 3.293 7.293 4.707"/>
-          </svg>
-        </div>
-        <span className='dotContainer'>
-          {
-            numberOfSlidesArray.map((index)=>{
-              return <button className={index === activeSlide ? "dot active" : "dot"} onClick={()=> goToSlide(index)}>○</button>
-            })
-    
-          }
-            
-        </span>
+
         <div className="carousel" ref={carouselRef} onScroll={handleScroll}>
           <div className="image one"></div>
           <div className="image two"></div>
@@ -68,7 +48,20 @@ function App() {
           <div className="image four"></div>
           <div className="image five"></div>
         </div>
-      </div>
+
+        <div className="buttonContainer">
+          <button className="slideButton" onClick={prevSlide} style={{visibility: activeSlide === 0 ? "hidden" : "visible"}}>◄</button>
+          <button className="slideButton" onClick={nextSlide} style={{visibility: xScrollPosition === xScrollWidth - imageWidth ? "hidden" : "visible"}}>►</button>
+        </div>
+
+        <div className='dotContainer'>
+          {
+            numberOfSlidesArray.map((index)=>{
+              return <button className={index === activeSlide ? "dot active" : "dot"} onClick={()=> goToSlide(index)}>•</button>
+            })
+          }
+        </div>
+
     </div>
   );
 }
